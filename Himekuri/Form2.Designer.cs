5a49dab0dbc1c4792eda0385071a3deaf62a3f03
A.Sugiwaka <sugiwaka@mtj.biglobe.ne.jp>
2025-09-01 12:43:42 +0900

    Initial commit

---
+++ Himekuri/Form2.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Himekuri
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(727, 297);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "人生楽あれば楽だけ\r\nにんじんだもの\r\n黙って座れば〇〇〇\r\n8割でやめる。8割でやめない。           そこまで重要でないことは8割の完成度で満足しよう" +
+    "。大切なことは8割の完成度で満足しないようにしよう\r\nあきらめたらそこで試合終了だよ\r\n何かを始めるのに遅すぎるということはない。だが、焦ったほうがいい。\r\n努" +
+    "力をしても成功するとは限らん。だが、成功した者は皆すべからく努力しておる！！";
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(621, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "リスト更新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 357);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "金言格言戯言リスト";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Himekuri/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Himekuri
+{
+    public partial class Form2 : Form
+    {
+        // フォーム1への参照を保持するフィールド★
+        private Form1 form1;
+
+        // コンストラクタでフォーム1への参照を受け取る★
+        public Form2(Form1 form1)
+        {
+            InitializeComponent();
+            this.form1 = form1;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var filePath = "D:\\Tools\\List.txt";//".\\List.txt";
+                textBox1.Text = File.ReadAllText(filePath);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー");
+            }
+        }
+
+        // フォーム2の「リスト更新」ボタンをクリックしたとき
+        // フォーム2のテキストボックスの内容を
+        // テキストファイルに書き出し、
+        // フォーム1のリストボックスの内容を更新します
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var filePath = "D:\\Tools\\List.txt";//".\\List.txt";
+                string[] items = textBox1.Text.Split('\n');
+                File.WriteAllLines(filePath, items);
+                form1.UpdateListBox(items);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー");
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+    }
+}
